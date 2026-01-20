1d4d8c14ddd72bc12e7d305b13d3b61012aeab49
Nina <calmon.nina01@gmail.com>
2026-01-19 22:53:45 -0300

    fix: workaround to avoid showing spell choice that does not do anything. STILL NOT 100% WORKING

--- select_spell.gd
+++ select_spell.gd
@@ -3,6 +3,8 @@ extends HBoxContainer
 @onready var canvas_layer: CanvasLayer = %CanvasLayer
 @onready var animation_canvas_layer: CanvasLayer = %AnimationCanvasLayer
 @onready var book_animation: AnimatedSprite2D = %BookAnimation
+@onready var choice_l: Button = %ChoiceL
+@onready var choice_r: Button = %ChoiceR
 
 var possible_spell_options: Array[EventSpell] = [SoulPiercer.new(), VampiricGoblet.new()]
 var spell_left: EventSpell
@@ -14,13 +16,15 @@ func _ready():
 	EventBus.player_level_up.connect(_on_player_level_up)
 
 func _on_choice_l_pressed() -> void:
-	EventBus.new_spell_added.emit(self.spell_left)
+	if self.spell_left != null:
+		EventBus.new_spell_added.emit(self.spell_left)
 	## APPEND OPTION NOT CHOSEN TO POSSIBLE SPELL OPTIONS ARRAY AGAIN
 	on_selected_choice(self.spell_right)
 
 
 func _on_choice_r_pressed() -> void:
-	EventBus.new_spell_added.emit(self.spell_right)
+	if self.spell_right != null:
+		EventBus.new_spell_added.emit(self.spell_right)
 	## APPEND OPTION NOT CHOSEN TO POSSIBLE SPELL OPTIONS ARRAY AGAIN
 	on_selected_choice(self.spell_left)
 
@@ -32,12 +36,24 @@ func _on_player_level_up(_level: int):
 		var random_num_array_bound_left = randi() % possible_spell_options.size()
 		spell_left = possible_spell_options.get(random_num_array_bound_left)
 		possible_spell_options.remove_at(random_num_array_bound_left)
+		choice_l.show()
+	else:
+		spell_left = null
+		choice_l.hide()
+		
 
 	if possible_spell_options.size() != 0:
 		var random_num_array_bound_right = randi() % possible_spell_options.size()
 		spell_right = possible_spell_options.get(random_num_array_bound_right)
 		possible_spell_options.remove_at(random_num_array_bound_right)
-	
+		choice_r.show()
+	else:
+		spell_right = null
+		choice_r.hide()
+
+	## REALLY IMPORTANT!!!!!!!!!!!!!!!!!!!! OOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOOO
+	## DO SOMETHING WHEN THERE ARE NOT TWO OPTIONS OF SPELLS TO CHOOSE
+	## TELL PLAYER OR SOMETHING
 	animation_canvas_layer.show()
 	book_animation.play()
 	book_animation.animation_finished.connect(_on_book_animation_finished)
