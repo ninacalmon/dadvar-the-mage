dc32cd64e3681c070b4458b5d6ba69c59d16da07
Nina <calmon.nina01@gmail.com>
2026-01-29 20:25:07 -0300

    feat: add procedural generation to build world map. add forest tile set. use vertex tint received on glowing shader to avoid inverting particle colors on ghost

---
+++ proc_gen_world_map.gd
@@ -0,0 +1,39 @@
+extends Node2D
+
+@export var noise_woods_texture: NoiseTexture2D
+var noise: Noise
+
+var width: float = 500
+var height: float = 500
+var noise_value_arr = []
+
+@onready var tile_map_layer: TileMapLayer = $"../y Sorted Layer/OverTileMapLayer"
+var source_id = 1
+var woods_atlas = [Vector2i(3, 2), Vector2i(1, 2), Vector2i(6, 2), Vector2i(10, 2), Vector2i(5, 4)]
+var ground_atlas = [Vector2i(2,0), Vector2i(3, 0), Vector2i(4, 0), Vector2i(5, 0)]
+var blank_atlas = Vector2i(0, 0)
+
+func _ready() -> void:
+	randomize()
+	noise = noise_woods_texture.noise
+	generate_world()
+	
+func generate_world():
+	for x in range(-width/2, width/2):
+		for y in range(-height/2, height/2):
+			if x % 2 == 0 and y % 2 == 0:
+				var noise_value: float = noise.get_noise_2d(x, y)
+				noise_value_arr.append(noise_value)
+				#print("max ", noise_value_arr.max())
+				#print("min ", noise_value_arr.min())
+				if noise_value <= -0.55:
+					# place woods
+					tile_map_layer.set_cell(Vector2(x, y), source_id, woods_atlas.pick_random())
+					
+				elif noise_value > -0.55 and noise_value < -0.45:
+					# place grass / rocks
+					tile_map_layer.set_cell(Vector2(x, y), source_id, ground_atlas.pick_random())
+					
+				elif noise_value > -0.45:
+					# place nothing
+					tile_map_layer.set_cell(Vector2(x, y), source_id, blank_atlas)
