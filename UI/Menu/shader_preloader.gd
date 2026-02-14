498425890911f0dbdeda14881c372be8493e48cb
Nina <calmon.nina01@gmail.com>
2026-02-14 15:06:16 -0300

    Better shader preload logic. Delete useless folder

--- UI/Menu/shader_preloader.gd
+++ UI/Menu/shader_preloader.gd
@@ -4,22 +4,24 @@ func _enter_tree() -> void:
 	var mobs = [$GoblinBoss, $"Mega Cerberus", $Seraphim, $Banshee, $GargoyleBody2D, $Ghost, $Goblin, $Skeleton]
 	for mob in mobs:
 		mob.behaviour_module.is_shader_preload = true
-		
+
+func _ready() -> void:
 	var explosion_gpu_partcles: GPUParticles2D = $ExplosionGPUPartcles
 	explosion_gpu_partcles.emitting = true
-	await get_tree().process_frame
-	explosion_gpu_partcles.emitting = false
 
 	var fire_gpu_partcles: GPUParticles2D = $FireGPUPartcles
 	fire_gpu_partcles.emitting = true
-	await get_tree().process_frame
-	fire_gpu_partcles.emitting = false
 
 	var player_body_2d: CharacterBody2D = $PlayerBody2d
 	var player_area: Player = $PlayerBody2d/PlayerArea
 
 	player_body_2d.is_shader_preload = true
 	player_area.is_shader_preload = true
-	await get_tree().process_frame
+
+	for i in range(3):
+		await RenderingServer.frame_post_draw
+
+	explosion_gpu_partcles.emitting = false
+	fire_gpu_partcles.emitting = false
 
 	self.queue_free()
